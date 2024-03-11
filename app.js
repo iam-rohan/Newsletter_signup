c253abcc2b7d2d09235d5cbde5f64eb68630e609
Rohan Acharya <rohanacharya42@gmail.com>
2024-03-11 20:37:53 +0545

    Few styling twicks.

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function (req, res) {
-  const fName = req.body.firstName;
-  const lName = req.body.lastName;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
   const email = req.body.email;
 
-  console.log(fName); // For testing purposes
+  console.log(firstName, lastName, email); // For testing purposes
 });
 
 app.listen(port, function () {
   console.log("Running on port %d", port);
 });
+
+// API key: 9ffc3bb23e643f81f5854a24d237e502-us22
