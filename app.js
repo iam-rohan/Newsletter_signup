d4393c1a7d67d1a279342fefe1a7a74b72919a39
Rohan Acharya <rohanacharya42@gmail.com>
2024-03-12 23:57:16 +0545

    Removed the node_module and configured the project to run on web.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,12 +17,45 @@ app.post("/", function (req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-  console.log(firstName, lastName, email); // For testing purposes
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us22.api.mailchimp.com/3.0/lists/d45bac6809";
+  const options = {
+    method: "POST",
+    auth: "notdecided:9ffc3bb23e643f81f5854a24d237e502-us22",
+  };
+
+  const requests = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  requests.write(jsonData);
+  requests.end();
 });
 
-app.listen(port, function () {
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(port || 3000, function () {
   console.log("Running on port %d", port);
 });
-
-// API key: 9ffc3bb23e643f81f5854a24d237e502-us22
